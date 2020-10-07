30ce391ce3109add598045a5f605bbb38f5ea15e
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2020-10-07 01:53:58 -0700

    Return an empty Vec if available_ports() fails
    Previously, we returned a Result from rfe::signal_generators() and
    rfe::spectrum_analyzers() in order to pass along any errors from
    serialport::available_ports(). Passing along such an uncommon error
    isn't worth the extra effort of dealing with a Result type. So now, if
    serialport::available_ports() returns an error, we consider it to be the
    same thing as if no RF Explorers were found.

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,21 @@ pub use rf_explorer::{Error, Result, RfExplorer};
 pub use signal_generator::SignalGenerator;
 pub use spectrum_analyzer::SpectrumAnalyzer;
 
-use serialport::{self, Error as SerialPortError};
+use serialport;
 use std::convert::TryFrom;
 
-pub fn signal_generators() -> std::result::Result<Vec<SignalGenerator>, SerialPortError> {
-    Ok(serialport::available_ports()?
+pub fn signal_generators() -> Vec<SignalGenerator> {
+    serialport::available_ports()
+        .unwrap_or_default()
         .iter()
         .filter_map(|serial_port_info| SignalGenerator::try_from(serial_port_info).ok())
-        .collect())
+        .collect()
 }
 
-pub fn spectrum_analyzers() -> std::result::Result<Vec<SpectrumAnalyzer>, SerialPortError> {
-    Ok(serialport::available_ports()?
+pub fn spectrum_analyzers() -> Vec<SpectrumAnalyzer> {
+    serialport::available_ports()
+        .unwrap_or_default()
         .iter()
         .filter_map(|serial_port_info| SpectrumAnalyzer::try_from(serial_port_info).ok())
-        .collect())
+        .collect()
 }
