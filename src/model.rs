f12f979b9f1ee647b40b7c60d2f58b6c18375477
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2020-10-09 00:31:01 -0700

    Remove "Rfe" from the start of model names

--- src/model.rs
+++ src/model.rs
@@ -5,18 +5,18 @@ use std::{convert::TryFrom, str, str::FromStr};
 #[derive(Debug, Copy, Clone, TryFromPrimitive, Eq, PartialEq)]
 #[repr(u8)]
 pub enum Model {
-    Rfe433 = 0,
-    Rfe868 = 1,
-    Rfe915 = 2,
-    RfeWSub1G = 3,
-    Rfe2400 = 4,
-    RfeWSub3G = 5,
-    Rfe6G = 6,
-    RfeWSub1GPlus = 10,
-    RfeAudioPro = 11,
-    Rfe2400Plus = 12,
-    Rfe4GPlus = 13,
-    Rfe6GPlus = 14,
+    FourThreeThreeM = 0,
+    EightSixEightM = 1,
+    NineOneFiveM = 2,
+    WSubOneG = 3,
+    TwoPointFourG = 4,
+    WSubThreeG = 5,
+    SixG = 6,
+    WSub1GPlus = 10,
+    ProAudio = 11,
+    TwoPointFourGPlus = 12,
+    FourGPlus = 13,
+    SixGPlus = 14,
 }
 
 impl FromStr for Model {
