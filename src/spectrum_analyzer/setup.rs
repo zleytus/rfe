768c72d5c0958cdcba36898ad35ea60d7a2b4e8c
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2020-10-12 02:55:59 -0700

    Rename rfe::Model variants

--- src/spectrum_analyzer/setup.rs
+++ src/spectrum_analyzer/setup.rs
@@ -33,7 +33,7 @@ mod tests {
     #[test]
     fn accept_wsub1g_setup() {
         let setup = Setup::try_from(b"#C2-M:003,255,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSubOneG);
+        assert_eq!(setup.main_model(), Model::RfeWSub1G);
         assert_eq!(setup.expansion_model(), None);
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
@@ -41,7 +41,7 @@ mod tests {
     #[test]
     fn accept_24g_setup() {
         let setup = Setup::try_from(b"#C2-M:004,255,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::TwoPointFourG);
+        assert_eq!(setup.main_model(), Model::Rfe24G);
         assert_eq!(setup.expansion_model(), None);
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
@@ -49,31 +49,31 @@ mod tests {
     #[test]
     fn accept_ism_combo_setup() {
         let setup = Setup::try_from(b"#C2-M:003,004,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSubOneG);
-        assert_eq!(setup.expansion_model(), Some(Model::TwoPointFourG));
+        assert_eq!(setup.main_model(), Model::RfeWSub1G);
+        assert_eq!(setup.expansion_model(), Some(Model::Rfe24G));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
     #[test]
     fn accept_3g_combo_setup() {
         let setup = Setup::try_from(b"#C2-M:003,005,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSubOneG);
-        assert_eq!(setup.expansion_model(), Some(Model::WSubThreeG));
+        assert_eq!(setup.main_model(), Model::RfeWSub1G);
+        assert_eq!(setup.expansion_model(), Some(Model::RfeWSub3G));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
     #[test]
     fn accept_6g_combo_setup() {
         let setup = Setup::try_from(b"#C2-M:006,005,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::SixG);
-        assert_eq!(setup.expansion_model(), Some(Model::WSubThreeG));
+        assert_eq!(setup.main_model(), Model::Rfe6G);
+        assert_eq!(setup.expansion_model(), Some(Model::RfeWSub3G));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
     #[test]
     fn accept_wsub1g_plus_setup() {
         let setup = Setup::try_from(b"#C2-M:010,255,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSub1GPlus);
+        assert_eq!(setup.main_model(), Model::RfeWSub1GPlus);
         assert_eq!(setup.expansion_model(), None);
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
@@ -81,24 +81,24 @@ mod tests {
     #[test]
     fn accept_ism_combo_plus_setup() {
         let setup = Setup::try_from(b"#C2-M:010,012,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSub1GPlus);
-        assert_eq!(setup.expansion_model(), Some(Model::TwoPointFourGPlus));
+        assert_eq!(setup.main_model(), Model::RfeWSub1GPlus);
+        assert_eq!(setup.expansion_model(), Some(Model::Rfe24GPlus));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
     #[test]
     fn accept_4g_combo_plus_setup() {
         let setup = Setup::try_from(b"#C2-M:010,013,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSub1GPlus);
-        assert_eq!(setup.expansion_model(), Some(Model::FourGPlus));
+        assert_eq!(setup.main_model(), Model::RfeWSub1GPlus);
+        assert_eq!(setup.expansion_model(), Some(Model::Rfe4GPlus));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
     #[test]
     fn accept_6g_combo_plus_setup() {
         let setup = Setup::try_from(b"#C2-M:010,014,XX.XXXX".as_ref()).unwrap();
-        assert_eq!(setup.main_model(), Model::WSub1GPlus);
-        assert_eq!(setup.expansion_model(), Some(Model::SixGPlus));
+        assert_eq!(setup.main_model(), Model::RfeWSub1GPlus);
+        assert_eq!(setup.expansion_model(), Some(Model::Rfe6GPlus));
         assert_eq!(setup.firmware_version(), "XX.XXXX".to_string());
     }
 
