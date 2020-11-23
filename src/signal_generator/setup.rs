8f9b35fafc183960ab56a69303aab40a1a02a0f3
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2020-11-23 09:33:17 -0800

    Add test for rfe::signal_generator::Setup

--- src/signal_generator/setup.rs
+++ src/signal_generator/setup.rs
@@ -33,3 +33,20 @@ impl crate::rf_explorer::Setup for Setup {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Message;
+    use crate::Model;
+
+    #[test]
+    fn accept_rfe_gen_setup() {
+        let setup = Setup::from_bytes(b"#C3-M:060,255,01.15\r\n".as_ref())
+            .unwrap()
+            .1;
+        assert_eq!(setup.main_model(), Model::RfeGen);
+        assert_eq!(setup.expansion_model(), None);
+        assert_eq!(setup.firmware_version(), "01.15");
+    }
+}
