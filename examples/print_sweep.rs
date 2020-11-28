eba1e50ccd81663d4aa44df3a6ab2d3fdb6fef40
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2020-11-28 15:37:30 -0800

    Print rfe::SpectrumAnalyzer in print_sweep example

--- examples/print_sweep.rs
+++ examples/print_sweep.rs
@@ -1,8 +1,8 @@
-use rfe::{self, RfExplorer};
+use rfe;
 
 fn main() {
     if let Some(mut spectrum_analyzer) = rfe::first_spectrum_analyzer() {
-        println!("{:?}", spectrum_analyzer.request_config());
+        println!("{:?}", spectrum_analyzer);
         println!("{:?}", spectrum_analyzer.get_sweep());
     } else {
         println!("No spectrum analyzers connected");
