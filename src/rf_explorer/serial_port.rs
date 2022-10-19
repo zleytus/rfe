0a4a42fc7d39af20a3fc37d4c49f8196503a3884
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2022-10-19 16:52:32 -0400

    Only connect to CU and not TTY devices on macOS

--- src/rf_explorer/serial_port.rs
+++ src/rf_explorer/serial_port.rs
@@ -1,6 +1,7 @@
 use super::Command;
 use serialport::{
     DataBits, FlowControl, Parity, SerialPort, SerialPortInfo, SerialPortType, StopBits,
+    UsbPortInfo,
 };
 use std::{
     io::{self, BufReader},
@@ -8,21 +9,39 @@ use std::{
 };
 use thiserror::Error;
 
+const SILICON_LABS_VID: u16 = 4_292;
+const CP210X_UART_BRIDGE_PID: u16 = 60_000;
+const RF_EXPLORER_BAUD_RATE: u32 = 500_000;
+
+fn is_rf_explorer_serial_port(port_type: &SerialPortType) -> bool {
+    matches!(
+        port_type,
+        SerialPortType::UsbPort(UsbPortInfo {
+            vid: SILICON_LABS_VID,
+            pid: CP210X_UART_BRIDGE_PID,
+            ..
+        })
+    )
+}
+
 pub(crate) fn open(port_info: &SerialPortInfo) -> ConnectionResult<SerialPortReader> {
-    let mut serial_port = {
-        let (port_type, port_name) = (&port_info.port_type, &port_info.port_name);
-        if let SerialPortType::UsbPort(_) = port_type {
-            Ok(serialport::new(port_name, 500_000)
-                .data_bits(DataBits::Eight)
-                .flow_control(FlowControl::None)
-                .parity(Parity::None)
-                .stop_bits(StopBits::One)
-                .timeout(Duration::from_secs(1))
-                .open()?)
-        } else {
-            Err(ConnectionError::NotAnRfExplorer)
-        }
-    }?;
+    // On macOS, serial devices show up in /dev twice as /dev/tty.devicename and /dev/cu.devicename
+    // For our purposes, we only want to connect to CU (Call-Up) devices
+    if cfg!(target_os = "macos") && !port_info.port_name.starts_with("/dev/cu.") {
+        return Err(ConnectionError::NotAnRfExplorer);
+    }
+
+    if !is_rf_explorer_serial_port(&port_info.port_type) {
+        return Err(ConnectionError::NotAnRfExplorer);
+    }
+
+    let mut serial_port = serialport::new(&port_info.port_name, RF_EXPLORER_BAUD_RATE)
+        .data_bits(DataBits::Eight)
+        .flow_control(FlowControl::None)
+        .parity(Parity::None)
+        .stop_bits(StopBits::One)
+        .timeout(Duration::from_secs(1))
+        .open()?;
     serial_port.write_all(Command::RequestConfig.as_ref())?;
     Ok(SerialPortReader::new(serial_port))
 }
