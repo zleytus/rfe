10f453e4b732b62477f833310492c2dff780dc34
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2022-10-26 19:26:28 -0400

    Move `Error` and `Result` to their own file

---
+++ src/rf_explorer/error.rs
@@ -0,0 +1,19 @@
+use std::{io, time::Duration};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("Invalid input: {}", .0)]
+    InvalidInput(String),
+
+    #[error("Invalid operation: {}", .0)]
+    InvalidOperation(String),
+
+    #[error(transparent)]
+    Io(#[from] io::Error),
+
+    #[error("Failed to complete the operation within the timeout duration ({} ms)", .0.as_millis())]
+    TimedOut(Duration),
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
--- src/rf_explorer/mod.rs
+++ src/rf_explorer/mod.rs
@@ -1,5 +1,6 @@
 mod command;
 mod device;
+mod error;
 mod frequency;
 mod message;
 mod model;
@@ -12,10 +13,10 @@ mod setup_info;
 
 pub(crate) use command::Command;
 pub use device::Device;
+pub use error::{Error, Result};
 pub use frequency::Frequency;
 pub use message::{Message, ParseFromBytes};
 pub use model::Model;
-pub(crate) use rf_explorer::RfeResult;
 pub use rf_explorer::{Error, RfExplorer};
 pub use screen_data::ScreenData;
 pub use serial_number::SerialNumber;
