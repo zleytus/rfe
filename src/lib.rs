0140b1b88c224bc104f0cf9e87c01c98476980f4
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2022-12-21 00:01:21 -0800

    Export `ScreenData` from root module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ pub mod common;
 pub mod signal_generator;
 pub mod spectrum_analyzer;
 
-pub use common::{Error, Frequency, Message, Model, Result, RfExplorer};
+pub use common::{Error, Frequency, Message, Model, Result, RfExplorer, ScreenData};
 pub use signal_generator::SignalGenerator;
 pub use spectrum_analyzer::SpectrumAnalyzer;
