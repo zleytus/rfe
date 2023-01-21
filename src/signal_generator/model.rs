c04aec6021c74d0b6be14ea4410ba0ba178093b9
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2023-01-21 01:45:02 -0500

    Change sig-gen models from `RfGen` to `Rfe6Gen`

--- src/signal_generator/model.rs
+++ src/signal_generator/model.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use num_enum::TryFromPrimitive;
 
 use crate::Frequency;
@@ -5,24 +7,33 @@ use crate::Frequency;
 #[derive(Debug, Copy, Clone, TryFromPrimitive, Eq, PartialEq)]
 #[repr(u8)]
 pub enum Model {
-    RfGen = 60,
-    RfGenExpansion = 61,
+    Rfe6Gen = 60,
+    Rfe6GenExpansion = 61,
 }
 
 impl Model {
     pub fn min_freq(&self) -> Frequency {
         match self {
-            Self::RfGen => 23_400_000,
-            Self::RfGenExpansion => 100_000,
+            Self::Rfe6Gen => 23_400_000,
+            Self::Rfe6GenExpansion => 100_000,
         }
         .into()
     }
 
     pub fn max_freq(&self) -> Frequency {
         match self {
-            Self::RfGen => 6_000_000_000,
-            Self::RfGenExpansion => 6_000_000_000,
+            Self::Rfe6Gen => 6_000_000_000,
+            Self::Rfe6GenExpansion => 6_000_000_000,
         }
         .into()
+    }
+}
+
+impl Display for Model {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Model::Rfe6Gen => write!(f, "6Gen"),
+            Model::Rfe6GenExpansion => write!(f, "6Gen Expansion"),
+        }
     }
 }
