deec8dcbad34738e693f2a6eacc50b4d5eae8ba8
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2023-06-12 15:02:57 -0700

    Implement `Default` for `RadioModule`

--- rfe/src/common/radio_module.rs
+++ rfe/src/common/radio_module.rs
@@ -1,12 +1,12 @@
 use crate::spectrum_analyzer::Model;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub enum RadioModule<M: Copy + Clone = Model> {
+pub enum RadioModule<M: Copy + Clone + Default = Model> {
     Main { model: M },
     Expansion { model: M },
 }
 
-impl<M: Copy + Clone> RadioModule<M> {
+impl<M: Copy + Clone + Default> RadioModule<M> {
     pub fn model(&self) -> M {
         match self {
             Self::Main { model, .. } => *model,
@@ -23,5 +23,13 @@ impl<M: Copy + Clone> RadioModule<M> {
 
     pub fn is_expansion(&self) -> bool {
         !self.is_main()
+    }
+}
+
+impl<M: Copy + Clone + Default> Default for RadioModule<M> {
+    fn default() -> Self {
+        Self::Main {
+            model: M::default(),
+        }
     }
 }
