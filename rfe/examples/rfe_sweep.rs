267c5c351a4ca7a3a446d25b9d1d8104953246df
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2023-07-19 11:09:56 -0400

    Print error when device not found in sweep example

--- rfe/examples/rfe_sweep.rs
+++ rfe/examples/rfe_sweep.rs
@@ -3,5 +3,7 @@ use rfe::RfExplorer;
 fn main() {
     if let Some(rfe) = RfExplorer::connect() {
         println!("{:?}", rfe.wait_for_next_sweep());
+    } else {
+        eprintln!("Failed to connect to an RF Explorer");
     }
 }
