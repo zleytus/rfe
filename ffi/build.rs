926cc4640795e373aae47785f63603281c44c329
Zach Leytus <22868934+zatchl@users.noreply.github.com>
2024-03-02 01:08:54 -0500

    Update `cbindgen` to 0.26

--- ffi/build.rs
+++ ffi/build.rs
@@ -1,28 +1,26 @@
 fn main() {
     let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    let config = cbindgen::Config {
-        language: cbindgen::Language::C,
-        documentation: false,
-        include_guard: Some("rfe_h".to_string()),
-        enumeration: cbindgen::EnumConfig {
-            rename_variants: cbindgen::RenameRule::ScreamingSnakeCase,
-            prefix_with_name: true,
-            ..Default::default()
-        },
-        parse: cbindgen::ParseConfig {
-            parse_deps: true,
-            include: Some(vec!["rfe".to_string()]),
-            ..Default::default()
-        },
-        cpp_compat: true,
-        defines: std::collections::HashMap::from_iter([
-            ("target_os = windows".to_string(), "_WIN32".to_string()),
-            ("target_os = macos".to_string(), "__APPLE__".to_string()),
-            ("target_os = linux".to_string(), "__linux__".to_string()),
-        ]),
+    let mut config = cbindgen::Config::default();
+    config.language = cbindgen::Language::C;
+    config.documentation = false;
+    config.include_guard = Some("rfe_h".to_string());
+    config.enumeration = cbindgen::EnumConfig {
+        rename_variants: cbindgen::RenameRule::ScreamingSnakeCase,
+        prefix_with_name: true,
         ..Default::default()
     };
+    config.parse = cbindgen::ParseConfig {
+        parse_deps: true,
+        include: Some(vec!["rfe".to_string()]),
+        ..Default::default()
+    };
+    config.cpp_compat = true;
+    config.defines = std::collections::HashMap::from_iter([
+        ("target_os = windows".to_string(), "_WIN32".to_string()),
+        ("target_os = macos".to_string(), "__APPLE__".to_string()),
+        ("target_os = linux".to_string(), "__linux__".to_string()),
+    ]);
 
     cbindgen::generate_with_config(crate_dir, config)
         .expect("Unable to generate bindings")
